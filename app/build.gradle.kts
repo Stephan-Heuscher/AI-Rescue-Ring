3c5a0122ace654b1b14a2040351ac80e4820dfee
Stephan Heuscher <x+git@yic.ch>
2025-11-02 17:07:08 +0100

    build: Bump version to 1.1.0
    - versionCode: 5  6
    - versionName: 1.0.4  1.1.0
    - Includes rescue ring mode and keyboard avoidance features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ch.heuscher.back_home_dot"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
