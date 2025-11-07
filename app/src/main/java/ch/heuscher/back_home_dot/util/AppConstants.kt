9012ce0951f801064a50079c164c6f10a3ebb079
Stephan Heuscher <x+git@yic.ch>
2025-11-07 19:59:42 +0100

    Set default dot color to blue

--- app/src/main/java/ch/heuscher/back_home_dot/util/AppConstants.kt
+++ app/src/main/java/ch/heuscher/back_home_dot/util/AppConstants.kt
@@ -68,7 +68,7 @@ object AppConstants {
     const val KEY_TAP_BEHAVIOR = "tap_behavior"
 
     // Default values
-    const val DEFAULT_COLOR = -0xBBBC // Blue: 0xFF2196F3
+    const val DEFAULT_COLOR = 0xFF2196F3.toInt() // Blue
     const val DEFAULT_ENABLED = false
     const val DEFAULT_KEYBOARD_AVOIDANCE = true
     const val DEFAULT_RESCUE_RING = false
